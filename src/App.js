6b4ecea5cf73c610bcf8bf4be34b51f8747a5425
limozaza <zakboite@gmail.com>
2018-05-27 13:45:43 +0200

    state initialisation

--- src/App.js
+++ src/App.js
@@ -4,13 +4,23 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    personnes : [
+        {name:"Zakaria", age:"32"},
+        {name:"Kassym", age:"2"},
+        {name:"Adil", age:"17"}
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Bonjour React</h1>
-        <Person name="Zakaria" age="32"/>
-        <Person name="Kassym" age="2">Mon fils</Person>
-        <Person name="Adil" age="17"/>
+        <button>Switch Name</button>
+        <Person name={this.state.personnes[0].name} age={this.state.personnes[0].age}/>
+        <Person name={this.state.personnes[1].name} age={this.state.personnes[1].age}>Mon fils</Person>
+        <Person name={this.state.personnes[2].name} age={this.state.personnes[2].age}/>
       </div>
     );
   }
